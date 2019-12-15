4b868ed9330e350e90171f1bcacb9ee3e4ec59c6
daniel <daniel@innodemneurosciences.com>
2019-12-15 15:02:54 -0500

    includes basic commands.

---
+++ wsl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update Ubuntu
+sudo apt-get update
+sudo apt-get upgrade
+
+# Change to dir
+cd /mnt/c/
+
+# Download VScode
+# https://code.visualstudio.com/
+
+# Download git
+# https://git-scm.com/download/win
+
+# Run
+# code .
+
+# Setup .bashprofile
