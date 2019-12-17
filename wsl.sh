a60510b1aef11eab751263cd52b729beb0aa2aec
daniel <daniel@innodemneurosciences.com>
2019-12-16 19:44:12 -0500

    completes WSL setup.

--- wsl.sh
+++ wsl.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Update Ubuntu
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade
 
 # Change to dir
 cd /mnt/c/
@@ -17,3 +16,27 @@ cd /mnt/c/
 # code .
 
 # Setup .bashprofile
+
+# Upgrade python
+
+
+# Upgrade pip
+sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
+
+# Install packages
+python3 -m pip install <package>
+
+#.sh file permission
+chmon +x in.sh
+
+# Change dir
+. in.sh
+
+#Setup bash profile
+sudo vim  ~/.profile
+
+#Copy .pem to ubuntu
+cp innodem.pem /home/danvargg
+
+#Key only readable by me
+chmod 400 innodem.pem
